8f3a0cb41ce5c136134bb3412023c606ea03052b
Manish Sharma <sharmakmanish1@gmail.com>
2025-06-20 17:31:31 +0530

    db updated

--- db.js
+++ db.js
@@ -2,7 +2,10 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool();
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // Required for Render PG
+});
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
